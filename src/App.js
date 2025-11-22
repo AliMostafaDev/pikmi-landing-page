26e6e8a740f023fb7d3268d9b95587604b81cb28
ali <alimostafaam2004@gmail.com>
2025-11-22 17:40:09 +0200

    feat: install Bootstrap 5 and React-Bootstrap dependencies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
+/**
+ * Pikmi Landing Page
+ * A community ride-sharing platform where people help each other
+ * and exchange Pikmi Coins (not real money)
+ */
 function App() {
   return (
-
+    <div className="App">
+      {/* Landing page sections will be added here */}
+    </div>
   );
 }
 
